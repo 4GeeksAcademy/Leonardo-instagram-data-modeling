b0bbe39458fc2dc4af893fd53732040c84a93dca
Leonardo Medina <leonardomedina1250@gmail.com>
2024-11-29 19:44:15 +0000

    Commit final Develop

--- src/models.py
+++ src/models.py
@@ -7,23 +7,101 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Usuario(Base):
+    __tablename__ = 'usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    nombre = Column(String(250), nullable=False)
+    apellido = Column(String(20), nullable=False)
+    edad = Column(Integer)
+    correo = Column(String(80), unique=True, nullable=False)
+    foto_perfil = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+# Personajes 
+class Personaje(Base):
+    __tablename__ = 'personaje'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nombre = Column(String(250), nullable=False)
+    apellido = Column(String(250), nullable=False)
+    raza = Column(String(250), nullable=False)
+    foto = Column(String(250), nullable=True)
+
+# Naves 
+class Naves(Base):
+    __tablename__ = 'naves'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250),nullable=False)
+    tipodenave = Column(String(250), nullable=True)
+    fecha_fabricacion = Column(String(250), nullable=False)
+    foto = Column(String(250), nullable=True)
+
+# Batallas 
+class Batalla(Base):
+    __tablename__ = 'batalla'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250),nullable=False)
+    vencedor = Column(String(250), nullable=True)
+    aliados = Column(String(250), nullable=False)
+    enemigos = Column(String(250), nullable=False)
+    foto = Column(String(250), nullable=True)
+
+# Armas 
+class Armas(Base):
+    __tablename__ = 'armas'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250),nullable=False)
+    fabricante = Column(String(250), nullable=True)
+    material = Column(String(250), nullable=False)
+    tipo_arma = Column(String(250), nullable=False)
+    foto = Column(String(250), nullable=True)
+
+# Planetas 
+class Planeta(Base):
+    __tablename__ = 'planeta'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250),nullable=False)
+    latitud = Column(String(250), nullable=False)
+    longitud = Column(String(250), nullable=False)
+    tipo_planeta = Column(String(250), nullable=False)
+    foto = Column(String(250), nullable=True)
+
+# Favoritos
+class Personaje_Favorito(Base):
+    __tablename__ = 'personaje_favorito'
+    id_fav_personaje = Column(Integer, primary_key=True)
+    id_usuario = Column(Integer, ForeignKey('usuario.id'), nullable=False)
+    id_personaje = Column(Integer, ForeignKey('personaje.id'), nullable=False)
+
+class Naves_Favorito(Base):
+    __tablename__ = 'naves_favorito'
+    id_fav_personaje = Column(Integer, primary_key=True)
+    id_usuario = Column(Integer, ForeignKey('usuario.id'), nullable=False)
+    id_naves = Column(Integer, ForeignKey('naves.id'), nullable=False)
+
+class Planeta_Favorito(Base):
+    __tablename__ = 'planeta_favorito'
+    id_fav_personaje = Column(Integer, primary_key=True)
+    id_usuario = Column(Integer, ForeignKey('usuario.id'), nullable=False)
+    id_planeta = Column(Integer, ForeignKey('planeta.id'), nullable=False)
+
+class Batalla_Favorito(Base):
+    __tablename__ = 'batalla_favorito'
+    id_fav_personaje = Column(Integer, primary_key=True)
+    id_usuario = Column(Integer, ForeignKey('usuario.id'), nullable=False)
+    id_batalla = Column(Integer, ForeignKey('batalla.id'), nullable=False)
+
+class Armas_Favorito(Base):
+    __tablename__ = 'armas_favorito'
+    id_fav_personaje = Column(Integer, primary_key=True)
+    id_usuario = Column(Integer, ForeignKey('usuario.id'), nullable=False)
+    id_armas = Column(Integer, ForeignKey('armas.id'), nullable=False)
+    
+    
+    
+    
+
+ 
 
     def to_dict(self):
         return {}
